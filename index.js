184089f9d601f7b676d3a17c0bc53b7f6115fc44
Jonathan Tran <trandingtopics@gmail.com>
2023-05-01 09:47:57 -0400

    format

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const loanMonthsElement = document.getElementById("loanMonths");
 //parseInt(prompt("Enter the loan length (in years): ")) * 12;
 
 const principal = parseFloat(principalElement.value);
-const interestRate = parseFloat(interestRate.value);
-const loanMonths = parseFloat(loanMonths.value);
+const interestRate = parseFloat(interestRateElement.value);
+const loanMonths = parseFloat(loanMonthsElement.value);
 
-const monthlyPayment = principal * (interestRate * ((1 + interestRate) ** loanLength)) / (((1 + interestRate) ** loanLength) - 1);
-const totalInterest = monthlyPayment * loanLength - principal;
+const monthlyPayment = principal * (interestRate * ((1 + interestRate) ** loanMonths)) / (((1 + interestRate) ** loanMonths) - 1);
+const totalInterest = monthlyPayment * loanMonths - principal;
 
-const message = `A $${principal.toFixed(2)} loan at ${interestRate*12*100}% interest for ${Math.floor(loanLength/12)} years would have a $${monthlyPayment.toFixed(2)}/mo payment with a total interest of $${totalInterest.toFixed(2)}`;
+const message = `A $${principal.toFixed(2)} loan at ${interestRate*12*100}% interest for ${Math.floor(loanMonths/12)} years would have a $${monthlyPayment.toFixed(2)}/mo payment with a total interest of $${totalInterest.toFixed(2)}`;
