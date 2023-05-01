b69abeb6916db2f52a0d8b197e87826536e09118
Jonathan Tran <trandingtopics@gmail.com>
2023-05-01 09:53:04 -0400

    calculate function

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const interestRateElement = document.getElementById("interestRate");
 //parseFloat(prompt("Enter the interest rate (%): ")) / 100 / 12;
 const loanMonthsElement = document.getElementById("loanMonths");
 //parseInt(prompt("Enter the loan length (in years): ")) * 12;
+const answerElement = document.getElementById("answer");
+
 
 const principal = parseFloat(principalElement.value);
 const interestRate = parseFloat(interestRateElement.value);
 const loanMonths = parseFloat(loanMonthsElement.value);
 
-const monthlyPayment = principal * (interestRate * ((1 + interestRate) ** loanMonths)) / (((1 + interestRate) ** loanMonths) - 1);
-const totalInterest = monthlyPayment * loanMonths - principal;
-
-const message = `A $${principal.toFixed(2)} loan at ${interestRate*12*100}% interest for ${Math.floor(loanMonths/12)} years would have a $${monthlyPayment.toFixed(2)}/mo payment with a total interest of $${totalInterest.toFixed(2)}`;
+function calculate() {
+    const monthlyPayment = principal * (interestRate * ((1 + interestRate) ** loanMonths)) / (((1 + interestRate) ** loanMonths) - 1);
+    const totalInterest = monthlyPayment * loanMonths - principal;
+    
+    const message = `A $${principal.toFixed(2)} loan at ${interestRate*12*100}% interest for ${Math.floor(loanMonths/12)} years would have a $${monthlyPayment.toFixed(2)}/mo payment with a total interest of $${totalInterest.toFixed(2)}`;
+    
+    answerElement.innerText = message;
+}
